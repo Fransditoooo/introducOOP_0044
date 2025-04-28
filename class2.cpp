810aec051b45addf1babfe6c11c4c718858e846b
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-04-28 10:47:34 +0700

    deklarasi kelas2

---
+++ class2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class mobil{
+    public:
+        string nama_Mobil;
+        string tipe_Mobil;
+    private:
+    int harga_Mobil;
+
+    public:
+        void printData(){
+            cout << "Masukkan nama mobil = ";
+            cin >> nama_Mobil;
+            cout << "Masukkan tipe mobil = ";
+            cin >> tipe_Mobil;
+            cout << "Masukkan harga mobil = ";
+            cin >> harga_Mobil;
+        }
+        void output_data(){
+            cout << "harga mobil = " << harga_Mobil << endl;
+        }
+};
+
+
