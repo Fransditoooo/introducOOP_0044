d7d920a058cb7f17caf39b3fb96fa4b5a3d31dfc
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-04-28 11:06:57 +0700

    deklarasi class3

---
+++ class3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class persegi_panjang{
+    private:
+        int panjang;
+        int lebar;
+        int luas;
+
+    public:
+        void input_Data(){
+            cout << "Masukkan panjang";
+            cin >> panjang;
+            cout << "Masukkan lebar";
+            cin >> lebar;
+            cout << "Masukkan luas";
+            cin >> luas;
+        }
+
+           
+}
